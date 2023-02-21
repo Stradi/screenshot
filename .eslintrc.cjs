50cf3743aa27614cf4716aa034ceed9375de1061
Batın Evirgen <batinevirgen@gmail.com>
2023-02-21 22:18:50 +0300

    build: add ESLint and Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:tailwindcss/recommended",
+  ],
+  plugins: ["tailwindcss"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+    }
+  ],
+  settings: {
+    react: {
+      version: "detect"
+    },
+    "import/resolver": {
+      node: {
+        paths: ['src'],
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+      }
+    },
+    "tailwindcss": {
+      "callees": ["cn"]
+    }
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  }
+}
