80b3cddbb9946001d5625d2f1f7420e57d0a42b2
Batın Evirgen <batinevirgen@gmail.com>
2023-02-24 14:04:52 +0300

    feat: use Switch component to change adaptiveness of the image

--- src/components/Editor/AspectRatioSelector.tsx
+++ src/components/Editor/AspectRatioSelector.tsx
@@ -1,8 +1,12 @@
+import { useEditor } from "../../context/EditorContext";
 import Button from "../ui/Button";
 import Popover from "../ui/Popover";
+import Switch from "../ui/Switch";
 import AspectRatioButton from "./AspectRatioButton";
 
 export default function AspectRatioSelector() {
+  const editor = useEditor();
+
   return (
     <Popover>
       <Popover.Trigger asChild>
@@ -10,9 +14,19 @@ export default function AspectRatioSelector() {
       </Popover.Trigger>
       <Popover.Content onOpenAutoFocus={(e) => e.preventDefault()}>
         <div className="flex flex-col gap-4">
-          <p className="border-b-2 border-b-neutral-200 text-sm text-neutral-400">
+          <p className="border-b-2 border-b-neutral-200 text-sm text-neutral-500">
             Select aspect ratio
           </p>
+          <div>
+            <Switch
+              name="adaptive"
+              label="Adaptive"
+              checked={editor.imageOptions.adaptive}
+              onCheckedChange={(value) =>
+                editor.setImageOptions({ ...editor.imageOptions, adaptive: value })
+              }
+            />
+          </div>
           <div className="grid grid-cols-4 gap-2">
             <AspectRatioButton aspectRatio="1:1" />
             <AspectRatioButton aspectRatio="4:3" />
