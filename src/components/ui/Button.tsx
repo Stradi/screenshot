836ca9eaff3bfda7346e1d0dc9e51474105af7f8
Batın Evirgen <batinevirgen@gmail.com>
2023-02-24 13:28:21 +0300

    feat: add active prop to Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { cn } from "../../utils/tw";
 
-type Props = React.ComponentPropsWithoutRef<"button"> & {
+export type ButtonProps = React.ComponentPropsWithoutRef<"button"> & {
   primary?: boolean;
   outline?: boolean;
   sm?: boolean;
   lg?: boolean;
   fullWidth?: boolean;
+  active?: boolean;
 };
 
-const Button = React.forwardRef<HTMLButtonElement, Props>(
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (
     {
       primary = true,
@@ -17,6 +18,7 @@ const Button = React.forwardRef<HTMLButtonElement, Props>(
       sm = false,
       lg = false,
       fullWidth = false,
+      active = false,
       // eslint-disable-next-line react/prop-types
       className,
       ...props
@@ -32,16 +34,15 @@ const Button = React.forwardRef<HTMLButtonElement, Props>(
           "focus:outline-none focus:ring-2 focus:ring-neutral-900 focus:ring-offset-2",
           "disabled:pointer-events-none disabled:opacity-50",
           "transition duration-100",
-
           primary &&
             "bg-neutral-900 text-white hover:bg-neutral-700 data-[state='open']:bg-neutral-700",
           outline &&
             "border border-neutral-300 bg-transparent text-black hover:bg-neutral-200 data-[state='open']:bg-neutral-200",
-
           !sm && !lg && "h-10 px-4 py-2",
           sm && "h-9 rounded-md px-2",
           lg && "h-11 rounded-md px-8",
           fullWidth && "w-full",
+          active && "bg-neutral-900 text-white hover:bg-neutral-700",
           className
         )}
         {...props}
