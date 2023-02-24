10450b3c841248a5ab8484bded2f3692774fcb07
Batın Evirgen <batinevirgen@gmail.com>
2023-02-24 13:16:27 +0300

    feat: add Popover component

---
+++ src/components/ui/Popover.tsx
@@ -0,0 +1,36 @@
+import * as _Popover from "@radix-ui/react-popover";
+import React from "react";
+import { cn } from "../../utils/tw";
+
+const Popover = _Popover.Root;
+
+const PopoverTrigger = _Popover.Trigger;
+
+const PopoverContent = React.forwardRef<
+  React.ElementRef<typeof _Popover.Content>,
+  React.ComponentPropsWithoutRef<typeof _Popover.Content>
+>((props, ref) => {
+  return (
+    <_Popover.Portal>
+      <_Popover.Content
+        ref={ref}
+        // eslint-disable-next-line tailwindcss/no-custom-classname
+        className={cn(
+          "mt-1 min-w-[384px] rounded-lg p-4",
+          "border border-neutral-200 bg-white shadow-lg",
+          "focus:outline-none",
+          "data-[state='open']:data-[side='top']:animate-fade-in-down",
+          "data-[state='open']:data-[side='bottom']:animate-fade-in-up"
+        )}
+        {...props}
+      />
+    </_Popover.Portal>
+  );
+});
+
+PopoverContent.displayName = "PopoverContent";
+
+export default Object.assign(Popover, {
+  Trigger: PopoverTrigger,
+  Content: PopoverContent,
+});
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,34 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in-down": {
+          from: {
+            opacity: "0",
+            transform: "translateY(-2px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-in-up": {
+          from: {
+            opacity: "0",
+            transform: "translateY(2px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.2s ease-out",
+        "fade-in-up": "fade-in-up 0.2s ease-out",
+      },
+    },
   },
   plugins: [],
 };
