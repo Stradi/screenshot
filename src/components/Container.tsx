3ef761d390abbb9f5215dc1ec1bd7564efdacfda
Batın Evirgen <batinevirgen@gmail.com>
2023-02-26 13:36:30 +0300

    chore: lint and format

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,10 +1,10 @@
-import { cn } from '../utils/tw';
+import { cn } from "../utils/tw";
 
-type Props = React.ComponentPropsWithoutRef<'div'>;
+type Props = React.ComponentPropsWithoutRef<"div">;
 
 export default function Container({ className, children, ...props }: Props) {
   return (
-    <div className={cn('container mx-auto max-w-6xl', className)} {...props}>
+    <div className={cn("container mx-auto max-w-6xl", className)} {...props}>
       {children}
     </div>
   );
--- src/components/ui/CoordinateInput.tsx
+++ src/components/ui/CoordinateInput.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import useWindowSize from "../../hooks/useWindowSize";
 import { clamp, mapRange } from "../../utils/math";
 import { cn } from "../../utils/tw";
